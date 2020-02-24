62a146d40de297fc04fdadd5066ebe5fdd372393
mukund agrawal <mukund.agrawal_cs18@gla.ac.in>
2020-02-24 16:13:53 +0530

    person fields initialised

---
+++ src/person/Person.java
@@ -0,0 +1,10 @@
+package person;
+
+import java.util.ArrayList;
+
+public class Person {
+    private String f_name;
+    private String l_name;
+    private ArrayList<String> p_number ;
+    private String email ;
+}
