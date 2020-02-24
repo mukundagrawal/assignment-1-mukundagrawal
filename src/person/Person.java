2e52cfa817919bfcc31843d7f27ab1fbd9d02e58
mukund agrawal <mukund.agrawal_cs18@gla.ac.in>
2020-02-24 16:15:14 +0530

    constructor made of class Person

--- src/person/Person.java
+++ src/person/Person.java
@@ -7,4 +7,14 @@ public class Person {
     private String l_name;
     private ArrayList<String> p_number ;
     private String email ;
+
+    public Person(String f_name, String l_name, ArrayList<String> p_number, String email) {
+        this.f_name = f_name;
+        this.l_name = l_name;
+        this.p_number = p_number;
+        this.email = email;
+    }
+
+
+
 }
