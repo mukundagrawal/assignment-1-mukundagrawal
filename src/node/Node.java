9a281e55ace1b2420f50bfe58f01642e262496ac
mukund agrawal <mukund.agrawal_cs18@gla.ac.in>
2020-02-24 16:46:10 +0530

    getter and setter created created of class Node

--- src/node/Node.java
+++ src/node/Node.java
@@ -12,4 +12,27 @@ public class Node {
         this.data = data;
     }
 
+    public Person getData() {
+        return data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public Node getPrevious() {
+        return previous;
+    }
+
+    public void setData(Person data) {
+        this.data = data;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    }
 }
