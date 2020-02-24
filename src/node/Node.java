4e94fdb62a31100cfa0a28bd72be5a073bc25c31
mukund agrawal <mukund.agrawal_cs18@gla.ac.in>
2020-02-24 16:44:15 +0530

    constructor created of class Node

--- src/node/Node.java
+++ src/node/Node.java
@@ -3,8 +3,13 @@ package node;
 import person.Person;
 
 public class Node {
+
     private Person data;
     private Node next;
     private Node previous;
+
+    public Node(Person data) {
+        this.data = data;
+    }
 
 }
