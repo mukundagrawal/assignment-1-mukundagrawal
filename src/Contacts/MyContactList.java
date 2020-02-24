43cc3f0c7561f17cf91f8b16dc4c75a82b73a9af
mukund agrawal <mukund.agrawal_cs18@gla.ac.in>
2020-02-24 16:49:02 +0530

    fields initialize  of class MyContactList

--- src/Contacts/MyContactList.java
+++ src/Contacts/MyContactList.java
@@ -1,7 +1,31 @@
 package Contacts;
-
+import java.util.regex.*;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import node.Node;
+import person.Person;
 
 
     public class MyContactList {
+        Scanner sc = new Scanner(System.in);
+
+        private int size;
+        private Node head;
+        private Node tail;
+
+        MyContactList()
+        {
+            head=null;
+            size=0;
+        }
+
+        public boolean isEmpty()
+        {
+            if(size==0)
+                return true;
+
+            return false;
+        }
 
     }
